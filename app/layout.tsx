a0e10e688c8c1ff735055e026befdfd1057aeb60
AJ Straight <ajstraight@AJs-MacBook-Pro.local>
2024-02-22 14:22:06 -0800

    removed dynamic routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/navigation/navbar";
 import { ThemeProvider } from "@/providers/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,7 +28,9 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <Navbar />
+            <Suspense>
+              <Navbar />
+            </Suspense>
             {children}
           </ThemeProvider>
         </body>
